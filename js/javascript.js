3ab601172ab60d02e0f0413af707155cad61ae2b
Adrian Istrate <adrian.istrate@student.unitbv.ro>
2021-02-09 20:29:30 +0200

    Added background images, scroll icon and text

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,21 @@
-$(document).ready(function() {
-  // jQuery code
-  $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
-  //////////////////////// detect scroll top or down
-    if ($('.smart-scroll').length > 0) { // check if element exists
-         var last_scroll_top = 0;
-        $(window).on('scroll', function() {
-            scroll_top = $(this).scrollTop();
-            if(scroll_top < last_scroll_top) {
-                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-            }
-            else {
-                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-            }
-            last_scroll_top = scroll_top;
-        });
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos < 300 ){
+    document.getElementById("scrollIcon").style.display = "block";
+  }
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+    if ( currentScrollPos > 300 ){
+      document.getElementById("scrollIcon").style.display = "none";
     }
-
-  
-}); 
+  }
+  prevScrollpos = currentScrollPos;
+}
+function easeIn() {
+  $('html,body').animate({
+        scrollTop: $(document.getElementById("firstPara")).offset().top},
+        2000);
+}
